8e061e32196b1d78f0891215b30adc031ccb7e50
VladimirB <vladimir.b@elementica.io>
2025-11-04 13:29:58 +0200

    1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AiFlightPlanningModule } from './modules/agents/aiFlightPlanning/aiFlightPlanning.module';
 
 @Module({
-  imports: [],
+  imports: [AiFlightPlanningModule],
   controllers: [AppController],
   providers: [AppService],
 })
